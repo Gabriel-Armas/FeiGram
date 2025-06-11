3ffd7a9de9846143e914cb838e6fb176a0754375
Juan Pablo Torres Ortiz <juan.torres.ortiz12@gmail.com>
2025-06-11 03:25:36 -0600

    Add: Post api is updated to tokens

--- services/post-api/src/upload_routes.py
+++ services/post-api/src/upload_routes.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, File, UploadFile, HTTPException
+from fastapi import APIRouter, File, UploadFile, HTTPException, Depends
 import cloudinary.uploader
 import src.cloudinary_config
+from src.auth import get_current_user  
 
 router = APIRouter()
 
 @router.post("/upload-image")
-async def upload_image(file: UploadFile = File(...)):
+async def upload_image(file: UploadFile = File(...),
+    user_id: str = Depends(get_current_user)):
     try:
         result = cloudinary.uploader.upload(file.file)
         return {
