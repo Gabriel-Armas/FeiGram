d8d5845bfb20fb556f9427465cdaf76c45dcb335
Yael <yaelalfredosalazaraguilar14@gmail.com>
2025-06-13 12:38:06 -0600

    fix: obj, bin and Likes was deleted

--- services/likes-api/src/Models/Likes.cs
+++
@@ -1,8 +0,0 @@
-namespace LikesApi.Models;
-
-public class Like
-{
-    public int Id { get; set; }
-    public string UserId { get; set; } = string.Empty;
-    public string PostId { get; set; } = string.Empty;
-}
